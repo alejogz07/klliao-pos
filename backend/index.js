a0a3fe3ee4bdae6bf433df78cf1cb75b996057ea
unknown <alejo.g200448@gmail.com>
2026-07-12 15:57:41 -0500

    feat: add stock entries API with transaction support and automatic stock update

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const categoriesRouter = require('./routes/categories');
 const sizesRouter = require('./routes/sizes');
 const productsRouter = require('./routes/products');
 const variantsRouter = require('./routes/variants');
+const stockEntriesRouter = require('./routes/stockEntries')
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,7 @@ app.use('/sizes', sizesRouter);
 app.use('/categories', categoriesRouter);
 app.use('/products', productsRouter);
 app.use('/variants', variantsRouter);
+app.use('/stock-entries', stockEntriesRouter)
 
 app.get('/', (req, res) => {
     res.json({ message: 'Klliao server running' });
