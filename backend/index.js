db3996baac6c32b1985d52c0c0ae5b5b9c5bb2e1
unknown <alejo.g200448@gmail.com>
2026-08-03 21:32:09 -0500

    feat: add POS view with product catalog, order management and sale resgistration

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const db = require('./database/db');
-const categoriesRouter = require('./routes/categories');
-const sizesRouter = require('./routes/sizes');
-const productsRouter = require('./routes/products');
-const variantsRouter = require('./routes/variants');
+const express = require('express')
+const cors = require('cors')
+const db = require('./database/db')
+const categoriesRouter = require('./routes/categories')
+const sizesRouter = require('./routes/sizes')
+const productsRouter = require('./routes/products')
+const variantsRouter = require('./routes/variants')
 const stockEntriesRouter = require('./routes/stockEntries')
 const salesRouter = require('./routes/sales')
 
-const app = express();
-const port = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.json());
-app.use('/sizes', sizesRouter);
-app.use('/categories', categoriesRouter);
-app.use('/products', productsRouter);
-app.use('/variants', variantsRouter);
-app.use('/stock-entries', stockEntriesRouter);
-app.use('/sales', salesRouter);
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+app.use(express.json())
+
+app.use('/categories', categoriesRouter)
+app.use('/sizes', sizesRouter)
+app.use('/products', productsRouter)
+app.use('/variants', variantsRouter)
+app.use('/stock-entries', stockEntriesRouter)
+app.use('/sales', salesRouter)
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Klliao server running' });
-});
+    res.json({ message: "K'lliao server running" })
+})
 
-app.listen(port, () => {
-    console.log(`Klliao server running at http://localhost:${port}`);
-});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
